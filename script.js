57f2b6dffdde51f73c0e767c418f8502079147d1
Vaibhav-G <127966735+Vaibhav-ghanekar@users.noreply.github.com>
2023-06-04 00:33:53 +0530

    updated script code

--- script.js
+++ script.js
@@ -9,17 +9,44 @@ window.addEventListener('scroll', function() {
     img_t.classList.toggle('sc-title', window.scrollY > 0);
     up_arrow.classList.toggle('sc-arrow',window.scrollY > 0);
   });
+  
+  function disableScroll() {
+    // Get the current scroll position
+    var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+    // Save the current scroll position
+    document.body.dataset.scrollPosition = scrollPosition;
+    // Add CSS to disable scroll
+    document.body.style.overflow = 'hidden';
+    document.body.style.position = 'fixed';
+    document.body.style.top = `-${scrollPosition}px`;
+  }
+  
+  // Enable scroll
+  function enableScroll() {
+    // Get the saved scroll position
+    var scrollPosition = parseInt(document.body.dataset.scrollPosition || '0', 10);
+    // Remove the CSS to enable scroll
+    document.body.style.overflow = '';
+    document.body.style.position = '';
+    document.body.style.top = '';
+    // Restore the saved scroll position
+    window.scrollTo(0, scrollPosition);
+  }
   let menu = document.getElementById("dropbox");
   let dropdown= document.getElementById("sidebar");
   let cross = document.getElementById("close");
+  
   function showBox(){
     dropdown.style.left="0px";
     dropdown.style.zIndex="2";
+    disableScroll();
   }
   function hideBox(){
     dropdown.style.left="-415px";
     dropdown.style.zIndex="0";
+    enableScroll();
   }
   function prompt(){
     alert("Currently working on it!!");
   }
+
